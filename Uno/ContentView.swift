e473cca4036e0cdf1cbe7b48fe43c314be89da35
Asher <aantrim246@cedarville.edu>
2024-08-28 14:15:56 -0400

    intial setup for uno app

--- Uno/ContentView.swift
+++ Uno/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        RoundedRectangle(cornerRadius: 15)
+            .padding()
+            .foregroundColor(.white)
+            .background(.red)
+        
     }
 }
 
