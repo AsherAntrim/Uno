4d2862273a199af9bdd2325419e652662955c927
Asher <aantrim246@cedarville.edu>
2024-08-28 14:11:49 -0400

    Initial Commit

---
+++ Uno/UnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnoApp.swift
+//  Uno
+//
+//  Created by Asher Antrim on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct UnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnoTests/UnoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnoTests.swift
+//  UnoTests
+//
+//  Created by Asher Antrim on 8/28/24.
+//
+
+import Testing
+@testable import Uno
+
+struct UnoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
